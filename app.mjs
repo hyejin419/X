7e469a9f46653f4aff8a523fe063b289551b65d8
Hyejin Kim <gpwls041932@gmail.com>
2025-04-24 15:40:56 +0900

    초기 세팅

--- app.mjs
+++ app.mjs
@@ -6,8 +6,8 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/posts", postsRoute);
-app.use("/auth", authRoute);
+app.use("/posts", postsRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
     res.sendStatus(404);
