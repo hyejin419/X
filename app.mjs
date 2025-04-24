87179647a2ab141fef8ee6c89bf2957fde70553b
Hyejin Kim <gpwls041932@gmail.com>
2025-04-24 12:46:55 +0900

    test

---
+++ app.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import postsRoute from "./router/posts.mjs";
+import authRoute from "./router/auth.mjs";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/posts", postsRoute);
+app.use("/auth", authRoute);
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+app.listen(8080);
