8a4a9f659fbee0f0eed2ac29a70794f3cd1469e3
Hyejin Kim <gpwls041932@gmail.com>
2025-04-24 22:02:23 +0900

    session 적용

--- router/auth.mjs
+++ router/auth.mjs
@@ -1,11 +1,19 @@
 import express from "express";
+import * as authController from "../controller/auth.mjs";
 
 const router = express.Router();
 
-//회원가입
+// 회원가입
+// POST
+// http://127.0.0.1:8080/auth/signup
+router.post("/signup", authController.signup);
 
-//로그인
+// 로그인
+// POST
+// http://127.0.0.1:8080/auth/login
+router.post("/login", authController.login);
 
-//로그인 유지
-
+// 로그인 유지
+router.get("/me", authController.checkSession);
+router.get("/logout", authController.logout);
 export default router;
