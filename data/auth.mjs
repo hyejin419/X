a02bd3d624258d60b4b7585e83c6761827b7e37f
hyejin419 <gpwls041932@gmail.com>
2025-05-12 09:59:42 +0900

    MongoDB 적용

--- data/auth.mjs
+++ data/auth.mjs
@@ -1,40 +1,31 @@
-import { db } from "../db/database.mjs";
+import MongoDb from "mongodb";
+import { getUsers } from "../db/database.mjs";
+const ObjectID = MongoDb.ObjectId;
 
 export async function createUser(user) {
-    const { userid, password, name, email, url } = user;
-    return db
-        .execute(
-            "insert into users (userid, password, name, email, url) values (?, ?, ?, ?, ?)",
-            [userid, password, name, email, url]
-        )
-        .then((result) => result[0].insertId);
+  return getUsers()
+    .insertOne(user)
+    .then((result) => result.insertedId.toString());
 }
 
 export async function login(userid, password) {
-    try {
-        console.log(" authRepository에서 로그인 요청:", userid, password);
-        const user = users.find(
-            (user) => user.userid === userid && user.password === password
-        );
-        if (!user) {
-            console.error(" 사용자를 찾을 수 없음:", userid);
-            return null; // res.status를 여기서 사용하지 않고 null 반환
-        }
-        return user; // 로그인 성공 시 사용자 객체 반환
-    } catch (error) {
-        console.error(" 로그인 중 오류 발생:", error);
-        throw new Error("서버 내부 오류 발생"); // 오류 발생 시 throw로 예외 전달
-    }
+  const user = users.find(
+    (user) => user.userid === userid && user.password === password
+  );
+  return user;
 }
 
 export async function findByUserid(userid) {
-    return db
-        .execute("select * from users where userid = ?", [userid])
-        .then((result) => result[0][0]);
+  return getUsers().find({ userid }).next().then(mapOptionalUser);
 }
 
-export async function findByid(idx) {
-    return db
-        .execute("select * from users where idx=?", [idx])
-        .then((result) => result[0][0]);
+export async function findByid(id) {
+  return getUsers()
+    .find({ _id: new ObjectID(id) })
+    .next()
+    .then(mapOptionalUser);
+}
+
+function mapOptionalUser(user) {
+  return user ? { ...user, id: user._id.toString() } : user;
 }
